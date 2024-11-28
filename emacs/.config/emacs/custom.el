0454d33387a416b6565c54f9541b28e5578f1f2f
Danilo Blera <danilo.cblera@protonmail.com>
2024-11-28 20:40:57 -0300

    Update emacs configuration
    Primarily move package configuration from `custom.el` into `init.el`
    with `use-package`.

--- emacs/.config/emacs/custom.el
+++ emacs/.config/emacs/custom.el
@@ -10,20 +10,40 @@
  '(custom-enabled-themes '(ef-summer))
  '(custom-safe-themes
    '("a57c3c53d60e99f39ad3272729810e0ea19a41c1fff2751888201695423714a9"
-     "39054dbdf1e3f5b36e91db507f86b4c709bc2bc99fbe5e2ffc600891261f50cd"
-     "8d820dc0b48dae30f0a76629665fc81f3d7dd150c008f48d8533646ba80188de"
-     "fef6645175d4c5f9d573daca2ba4d7efa781be10967024d1d8e6ef0c4aa71437"
-     "0a953c81f5798aa99cafbc4aa8a56d16827442400028f6c1eab0c43061ea331c"
-     "0279c1b81b569e46a4ee8e87e5e381568bf931257287191fd091f2948f7e2e8e"
-     "c3e62e14eb625e02e5aeb03d315180d5bb6627785e48f23ba35eb7b974a940af"
-     "dc96af3e6aaa9c96aa83d1a73a28a6d1dab58e376df1e51980b4fa9b256e9d7f"
-     "2b36236a9d02c9041efa7749d49cfbf6f55bb0a6674fae43f78a81367490e535"
-     "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af"
-     "87b82caf3ade09282779733fb6de999d683caf4a67a1abbee8b8c8018a8d9a6b"
-     "5e41864cbdd81b18d1fa62f09971a55a121a939238ca4c66faafcfcafb976c3e"
-     "01cad03be8c042a9941fda5a484280629ee2cc83fe084af6d19376c83141c91b"
-     "9d5124bef86c2348d7d4774ca384ae7b6027ff7f6eb3c401378e298ce605f83a"
-     "6752a763be62c7da5c70d04cd5f559c2f96479caa18809c54612cb918ba08708" default))
+     "b41d0a9413fb0034cea34eb8c9f89f6e243bdd76bccecf8292eb1fefa42eaf0a"
+     "2551f2b4bc12993e9b8560144fb072b785d4cddbef2b6ec880c602839227b8c7"
+     "a0e9bc5696ce581f09f7f3e7228b949988d76da5a8376e1f2da39d1d026af386"
+     "da69584c7fe6c0acadd7d4ce3314d5da8c2a85c5c9d0867c67f7924d413f4436"
+     "541282f66e5cc83918994002667d2268f0a563205117860e71b7cb823c1a11e9"
+     "3d9938bbef24ecee9f2632cb25339bf2312d062b398f0dfb99b918f8f11e11b1"
+     "71b688e7ef7c844512fa7c4de7e99e623de99a2a8b3ac3df4d02f2cd2c3215e7"
+     "b3ba955a30f22fe444831d7bc89f6466b23db8ce87530076d1f1c30505a4c23b"
+     "fae5872ff90462502b3bedfe689c02d2fa281bc63d33cb007b94a199af6ccf24"
+     "19b62f442479efd3ca4c1cef81c2311579a98bbc0f3684b49cdf9321bd5dfdbf"
+     "b9c002dc827fb75b825da3311935c9f505d48d7ee48f470f0aa7ac5d2a595ab2"
+     "cee5c56dc8b95b345bfe1c88d82d48f89e0f23008b0c2154ef452b2ce348da37"
+     "ffa78fc746f85d1c88a2d1691b1e37d21832e9a44a0eeee114a00816eabcdaf9"
+     "90185f1d8362727f2aeac7a3d67d3aec789f55c10bb47dada4eefb2e14aa5d01"
+     "4c16a8be2f20a68f0b63979722676a176c4f77e2216cc8fe0ea200f597ceb22e"
+     "aff0396925324838889f011fd3f5a0b91652b88f5fd0611f7b10021cc76f9e09"
+     "c038d994d271ebf2d50fa76db7ed0f288f17b9ad01b425efec09519fa873af53"
+     "6af300029805f10970ebec4cea3134f381cd02f04c96acba083c76e2da23f3ec"
+     "ac893acecb0f1cf2b6ccea5c70ea97516c13c2b80c07f3292c21d6eb0cb45239"
+     "b1791a921c4f38cb966c6f78633364ad880ad9cf36eef01c60982c54ec9dd088"
+     "d609d9aaf89d935677b04d34e4449ba3f8bbfdcaaeeaab3d21ee035f43321ff1"
+     "e85a354f77ae6c2e47667370a8beddf02e8772a02e1f7edb7089e793f4762a45"
+     "df39cc8ecf022613fc2515bccde55df40cb604d7568cb96cd7fe1eff806b863b"
+     "d6b369a3f09f34cdbaed93eeefcc6a0e05e135d187252e01b0031559b1671e97"
+     "59c36051a521e3ea68dc530ded1c7be169cd19e8873b7994bfc02a216041bf3b"
+     "296dcaeb2582e7f759e813407ff1facfd979faa071cf27ef54100202c45ae7d4"
+     "211621592803ada9c81ec8f8ba0659df185f9dc06183fcd0e40fbf646c995f23"
+     "ea4dd126d72d30805c083421a50544e235176d9698c8c541b824b60912275ba1"
+     "1ad12cda71588cc82e74f1cabeed99705c6a60d23ee1bb355c293ba9c000d4ac"
+     "cd5f8f91cc2560c017cc9ec24a9ab637451e36afd22e00a03e08d7b1b87c29ca"
+     "a3a71b922fb6cbf9283884ac8a9109935e04550bcc5d2a05414a58c52a8ffc47"
+     "ae20535e46a88faea5d65775ca5510c7385cbf334dfa7dde93c0cd22ed663ba0"
+     "00d7122017db83578ef6fba39c131efdcb59910f0fac0defbe726da8072a0729"
+     "36c5acdaf85dda0dad1dd3ad643aacd478fb967960ee1f83981d160c52b3c8ac" default))
  '(delete-by-moving-to-trash t)
  '(delete-selection-mode t)
  '(display-line-numbers-type t)
@@ -41,14 +61,8 @@
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
  '(mouse-wheel-progressive-speed nil)
- '(nyan-mode t)
- '(olivetti-body-width 100)
- '(olivetti-style 'fancy)
  '(org-startup-folded 'content)
- '(package-selected-packages
-   '(ace-window breadcrumb company direnv doom-themes ef-themes flycheck format-all jinx
-                nyan-mode olivetti paredit rainbow-delimiters shades-of-purple-theme sly
-                spacemacs-theme transpose-frame unicode-fonts vundo))
+ '(package-selected-packages nil)
  '(scroll-bar-mode nil)
  '(tab-bar-mode t)
  '(tab-bar-show 1)
--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,21 +1,85 @@
 ;;;; The journey into structured editing begins here
-;;; obs.: run `M-x package-install-selected-packages RET` to install the missing packages
-;;; indicated in the `custom.el` file
+;;;
+;;; obs.: this configuration was tested with version 31.0.50
 
-;; Add Melpa repositories
+;;; Packages
+;; add Melpa repositories
 (use-package package
   :config
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
 
-;; Specify the file for `customize` and friends
+(use-package ef-themes :ensure t)
+(use-package flycheck :ensure t :defer t)
+(use-package format-all :ensure t :defer t)
+(use-package shades-of-purple-theme :ensure t)
+(use-package transpose-frame :ensure t :defer t)
+(use-package unicode-fonts :ensure t)
+
+(use-package ace-window
+  :ensure t
+  :defer t
+  :bind ("M-o" . ace-window))
+
+(use-package avy
+  :ensure t
+  :defer t
+  :bind (("C-;" . avy-goto-char)
+         ("C-:" . avy-goto-char-2)))
+
+(use-package breadcrumb
+  :ensure t
+  :defer t
+  :hook (prog-mode . breadcrumb-mode))
+
+(use-package envrc
+  :ensure t
+  :defer t
+  :hook (after-init . envrc-global-mode))
+
+(use-package jinx
+  :ensure t
+  :defer t
+  :bind ("C-c t j" . jinx-mode)
+  :hook ((org-mode prog-mode text-mode) . jinx-mode))
+
+(use-package nyan-mode
+  :ensure t
+  :config (nyan-mode t))
+
+(use-package olivetti
+  :ensure t
+  :defer t
+  :bind ("C-c t o" . olivetti-mode)
+  :config (setq olivetti-body-width 100
+                olivetti-style 'fancy)
+  :hook ((org-mode prog-mode text-mode) . olivetti-mode))
+
+(use-package paredit
+  :ensure t
+  :defer t
+  :hook ((emacs-lisp-mode lisp-mode) . paredit-mode))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :defer t
+  :hook ((org-mode prog-mode text-mode) . rainbow-delimiters-mode))
+
+(use-package sly
+  :ensure t
+  :defer t
+  :config (setq inferior-lisp-program "/usr/bin/sbcl"))
+
+(use-package vundo
+  :ensure t
+  :defer t
+  :bind ("C-c d u" . vundo))
+
+;; General configuration
+;; specify the file for `customize` and friends
 (setq custom-file "~/.config/emacs/custom.el")
 (load custom-file)
 
-;; Install missing packages on startup -- check `package-selected-packages` in the custom
-;; file
-(package-install-selected-packages)
-
-;; Backup edited files under a centralized location
+;; backup edited files under a centralized location
 (setq backup-directory-alist '(("." . "~/.cache/emacs/backups"))
       backup-by-copying t
       version-control t
@@ -23,32 +87,20 @@
       kept-new-versions 6
       kept-old-versions 2)
 
-;; Configure the font
-(let ((name "UbuntuMono Nerd Font Mono-13"))
-  (add-to-list 'default-frame-alist `(font . ,name))
-  (set-face-attribute 'default t :font name))
+;; setting the default font
+(add-to-list 'default-frame-alist '(font . "Departure Mono-10"))
 
-;; Scroll line-by-line instead of in chunks
+;; scroll line-by-line instead of in chunks
 (setq scroll-step 1
       scroll-conservatively 10000)
 
-;; Hooks
-(dolist (mode '(rainbow-delimiters-mode whitespace-mode
-                olivetti-mode jinx-mode breadcrumb-mode))
-  (add-hook 'prog-mode-hook mode))
-
-(dolist (mode '(olivetti-mode jinx-mode))
-  (dolist (hook '(text-mode-hook org-mode-hook))
-    (add-hook mode hook)))
-
-(dolist (hook '(emacs-lisp-mode-hook lisp-mode-hook))
-  (add-hook hook 'enable-paredit-mode))
-
+;;; Hooks
+(add-hook 'prog-mode-hook #'whitespace-mode)
 (add-hook 'before-save-hook #'delete-trailing-whitespace)
 (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 (add-hook 'occur-hook (lambda () (switch-to-buffer-other-window "*Occur*")))
 
-;; Replacing default major modes with their respective tree-sitter versions
+;;; replace default major modes with their respective tree-sitter versions
 (setq major-mode-remap-alist
       '((bash-mode . bash-ts-mode)
         (c-mode . c-ts-mode)
@@ -57,40 +109,32 @@
         (toml-mode . toml-ts-mode)
         (yaml-mode . yaml-ts-mode)))
 
-;; The path to the common lisp subprocess for Sly to run
-(setq inferior-lisp-program "/usr/bin/sbcl")
-
-;; Keymaps
+;;; keymaps
 (keymap-global-set "C-c d b" #'fill-region)
 (keymap-global-set "C-c d c" #'comment-dwim)
 (keymap-global-set "C-c d o" #'occur)
 (keymap-global-set "C-c d r" #'repeat)
 (keymap-global-set "C-c d s" #'screenshot-svg)
-(keymap-global-set "C-c d u" #'vundo)
-(keymap-global-set "C-c t j" #'jinx-mode)
-(keymap-global-set "C-c t o" #'olivetti-mode)
 (keymap-global-set "C-c t r" #'view-mode)
 (keymap-global-set "C-x C-b" #'ibuffer)
-(keymap-global-set "C-;" #'avy-goto-char)
-(keymap-global-set "C-:" #'avy-goto-char-2)
-(keymap-global-set "M-o" #'ace-window)
 (global-unset-key (kbd "C-z"))  ; disable suspend
 
-;; Re-enable uppercase and lowercase region commands
+;; re-enable uppercase and lowercase region commands
 (put 'upcase-region 'disabled nil)      ; C-x C-u
 (put 'downcase-region 'disabled nil)    ; C-x C-l
 
-;; Re-enable narrowing region -- check C-x n {d,n,p,w}
+;; re-enable narrowing region -- check C-x n {d,n,p,w}
 (put 'narrow-to-region 'disabled nil)   ; C-x n n
 
-;; Features to disable
+;;; Features to disable
 (put 'enriched-mode 'disabled
      "this inserts lots of markup, which can be difficult to remove.")
 
 (put 'enriched-toggle-markup 'disabled
      "maybe avoid `enriched-mode' if you don't know what you're doing.")
 
-;; Save an SVG screenshot of emacs into `/tmp`
+;;; Custom functions
+;; save an SVG screenshot of emacs into `/tmp`
 (defun screenshot-svg ()
   "Save a screenshot of the current frame as an SVG image.  Saves to a temp file and puts
 the filename in the kill ring."
