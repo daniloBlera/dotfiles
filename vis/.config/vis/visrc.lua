54ce07cd964c388eed1a86b1496c54629c22e4ba
Danilo Blera <danilo.cblera@protonmail.com>
2026-01-18 18:10:28 -0300

    Add configuration for `vis`

---
+++ vis/.config/vis/visrc.lua
@@ -0,0 +1,19 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+    -- Your global configuration options
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
+    -- Your per window configuration options e.g.
+    -- vis:command('set number')
+    vis:command('set tabwidth 4')
+    vis:command('set autoindent on')
+    vis:command('set expandtab on')
+    vis:command('set relativenumber on')
+    vis:command('set showtabs on')
+    vis:command('set shownewlines on')
+    vis:command('set showspaces on')
+end)
+
